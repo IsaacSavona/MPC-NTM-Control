d8303178c300a6be57c3c18c953d9bda5518c350
Leon <Leon@DESKTOP-Q88U2A7.mshome.net>
2023-06-07 13:14:47 +0200

    added correct A and B calls

--- Rho_to_PhiGammaLambda.m
+++ Rho_to_PhiGammaLambda.m
@@ -1,12 +1,12 @@
-function [ Phi, Gamma, Lambda ] =  Rho_to_PhiGammaLambda(Rho1,Rho2,Rho3, A,B,C)
+function [ Phi, Gamma, Lambda ] =  Rho_to_PhiGammaLambda(Rho1,Rho2,Rho3, A,B,C, kappa,Ts,j_BS,zeta,tau_E,eta_CD,w_dep)
 %ABN2PhiGamma Summary of this function goes here
 %   Detailed explanation goes here
 %   A,B are function, C is a matrix
 N  = size(Rho1,1); 
 % nx = size(B,1);
 % nu = size(B,2);
-Phi1 = A(Rho1(1),Rho2(1));
-Gamma1 = B(Rho3(1));
+Phi1 = A(Rho1(1),Rho2(1), kappa,Ts,j_BS,zeta,tau_E);
+Gamma1 = B(Rho3(1), kappa,Ts,eta_CD,w_dep);
 Lambda1 = C;
 nx = size(Phi1,1);
 nu = size(Gamma1,1);
@@ -18,22 +18,22 @@ Phi = zeros(nx*N,nx);
 Phi(1,:) = Phi1;
 if N>1
     for j = 2:N
-        Phi(j,:) = Phi(j-1,:)*A(Rho1(j),Rho2(j));
+        Phi(j,:) = Phi(j-1,:)*A(Rho1(j),Rho2(j), kappa,Ts,j_BS,zeta,tau_E);
     end
 end
 
 % Gamma
 Gamma = zeros(nx*N,nu*N);
-Gamma(1:nx,1:nu) = B(Rho3(1));
+Gamma(1:nx,1:nu) = B(Rho3(1), kappa,Ts,eta_CD,w_dep);
 for i = 2:N %rows
     for j = 1:i %columns
         if i ~= j
             %Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = A(Rho1(i-j),Rho2(i-j))*B(Rho3(j));
-            Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = A(Rho1(i-j),Rho2(i-j))*Gamma((i-2)*nx+1:nx*(i-1),(j-1)*nu+1:j*nu);
+            Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = A(Rho1(i-j),Rho2(i-j), kappa,Ts,j_BS,zeta,tau_E)*Gamma((i-2)*nx+1:nx*(i-1),(j-1)*nu+1:j*nu);
             % Basic idea: new Gamma =
             % newA*previously_Multipled_A's_from_same_column
         else
-            Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = B(Rho3(j));
+            Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = B(Rho3(j), kappa,Ts,eta_CD,w_dep);
         end
         %Gamma((i-1)*nx+1:nx*i,(j-1)*nu+1:j*nu) = Gamma(j-1,:)*A(Rho1(i-j),Rho2(i-j))*B(Rho3(i));
     end
@@ -48,7 +48,7 @@ Lambda = zeros(nx*N,nC);
 Lambda(1:nx,1:nC) = Lambda1;
 for i=2:N
     %(current A)x(previous row) + c
-    Lambda((i*nx)-(nx-1):i*nx, 1:nC) = A(Rho1(i),Rho2(i))*Lambda(((i-1)*nx)-(nx-1):(i-1)*nx, 1:nC) + C ;
+    Lambda((i*nx)-(nx-1):i*nx, 1:nC) = A(Rho1(i),Rho2(i), kappa,Ts,j_BS,zeta,tau_E)*Lambda(((i-1)*nx)-(nx-1):(i-1)*nx, 1:nC) + C ;
 end
 
 end
