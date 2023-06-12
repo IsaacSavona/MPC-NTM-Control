ed210982b0ae5d24ac519ab01407bc4d068acf00
Leon <Leon@DESKTOP-Q88U2A7.mshome.net>
2023-06-12 18:51:22 +0200

    Working NMPC !!!

--- NMPC_1Sim.m
+++ NMPC_1Sim.m
@@ -17,12 +17,13 @@ mu0 = 4e-7*pi; % [N/A^2] vacuum permaebility
 Lq = 0.87;     % [m] q-gradient length scale
 B_pol = 0.97;  % [T] poloidal field
 m = 2;         % [#] poloidal mode number
-Cw = 1;        % [#] UNKNOWN!!
+Cw = 1e3;        % [#] UNKNOWN!!
 tau_A0 = 3e-6; % [s] Alfvèn time
 tau_w = 0.188; % [s] resistive wall time
 omega0 = 2*pi*420; % [rad/s] equilibrium frequency
 kappa = 16*mu0*Lq*rs^2/(0.82*tau_r*B_pol*pi);
 zeta = m*Cw*tau_A0^2*tau_w*a^3;
+
 Ts = 0.4;
 
 Const = [-4/3*(kappa*Ts*j_BS*w_sat)/(w_sat^2+w_marg^2); Ts*omega0/tau_E0];
@@ -49,14 +50,13 @@ max_freq = 5000*2*pi; % [rad/s]
 xmin = [min_width;min_freq]; % minimum on state vector
 xmax = [max_width;max_freq]; % maximum on state vector
 
-min_power = 0;    % [W]
-max_power = 2e6;  % [W]
+min_power = 0;    % [MW]
+max_power = 2;    % [MW]
 umin = min_power; % minimum on input vector
 umax = max_power; % maximum on input vector
 
 % Q
-%Q = 1*eye(nx);
-Q = 1*eye(2);
+Q = [1 0; 0 0];
 
 %% Nonlinear MPC - YALMIP (solver:fmincon)
 nx = 2;
@@ -68,18 +68,16 @@ constraints = [];
 for i = 1:N
     objective = objective + (x(:,i)-r)'*Q*(x(:,i)-r);
     % Use Nonlinear Dynamics
-    A = [(1+(4/3)*(kappa*Ts*j_BS*(rho1(x(:,i),w_marg))))+(Const(1,:)/x(1,i)),  0; ...
-        (-(rho2(x(:,i))*Ts)/(zeta)), (1-Ts/tau_E)+(Const(2,:)/x(2,i))];
-    B = [-(kappa*Ts*eta_CD/w_dep)*rho3(x(:,i), w_dep); 0];
-    constraints = [constraints, x(:,i+1)==A*x(:,i)+B*u(:,i)];
-    %constraints = [constraints, xmin<=x(:,i)<=xmax, umin<=u(:,i)<=umax];
+    Amat = A(rho1(x(:,i),w_marg), rho2(x(:,i)), kappa,Ts,j_BS,zeta,tau_E);
+    Bmat = B(rho3(x(:,i),w_dep), kappa,Ts,eta_CD,w_dep);
+    constraints = [constraints, x(:,i+1)==Amat*x(:,i)+Bmat*u(:,i)+Const];
+    constraints = [constraints, umin<=u(:,i)<=umax]; %xmin<=x(:,i)<=xmax,
 end
-%objective = objective + (x(:,N+1)-r)'*P*(x(:,N+1)-r);
-%constraints = [constraints, M_N*x(:,N+1)<=b_N];
 
 objective = objective + (x(:,N+1)-r)'*Q*(x(:,N+1)-r);
-constraints = [constraints, xmin<=x(:,N+1)<=xmax];
-MPC_Nonlinear = optimizer(constraints,objective,[],x(:,1),u);
+%constraints = [constraints, xmin<=x(:,N+1)<=xmax];
+options = sdpsettings('verbose',1);
+MPC_Nonlinear = optimizer(constraints,objective,options,x(:,1),u);
 % Check the solver of "MPC_Nonlinear": "Solver: FMINCON-STANDARD"
 
 k_sim = 20;
@@ -88,14 +86,34 @@ uk = zeros(nu,k_sim);
 tk = zeros(1,k_sim);
 for k = 1:k_sim
     tic
-    Uk = MPC_Nonlinear(xk(:,k));
+    [Uk,~,~,~,~] = MPC_Nonlinear(xk(:,k));
     tk(:,k) = toc;
     uk(:,k) = Uk(1:nu);
     % Use discrete nonlinear model
-    A = [(1+(4/3)*(kappa*Ts*j_BS*(rho1(xk(:,k),w_marg))))+(Const(1,:)/xk(1,k)),  0; (-(rho2(xk(:,k))*Ts)/(zeta))+(Const(1,:)/xk(1,k)), (1-Ts/tau_E)+(Const(2,:)/xk(2,k))];
-    B = [-(kappa*Ts*eta_CD/w_dep)*rho3(xk(:,k), w_dep); 0];
-    xk(:,k+1) = A*xk(:,k)+B*uk(:,k);
+    Amat = A(rho1(xk(:,k),w_marg), rho2(xk(:,k)), kappa,Ts,j_BS,zeta,tau_E);
+    Bmat = B(rho3(xk(:,k),w_dep), kappa,Ts,eta_CD,w_dep);
+    xk(:,k+1) = Amat*xk(:,k)+Bmat*uk(:,k)+Const;
 end
 data(1).x = xk;
 data(1).u = uk;
 data(1).t = tk;
+
+%% Plot output and input
+figure('Position', [100 100 1000 300])
+subplot(1,2,1);
+plot(Ts:Ts:k_sim*Ts,uk(:),'color',"#77AC30")
+xlabel('$t$ [s]','Interpreter','latex')
+ylabel('$P_{ECCD}$ [MW]','Interpreter','latex')
+title("Optimal Input")
+
+subplot(1,2,2);
+hold on
+yyaxis left
+plot(0:Ts:k_sim*Ts,xk(1,:)*100)
+ylabel('$\mathrm{w}$ [cm]','Interpreter','latex')
+yyaxis right
+plot(0:Ts:k_sim*Ts,xk(2,:)/(2*pi))
+ylabel('$\omega$ [Hz]','Interpreter','latex')
+hold off
+xlabel('$t$ [s]','Interpreter','latex')
+title("Output")
