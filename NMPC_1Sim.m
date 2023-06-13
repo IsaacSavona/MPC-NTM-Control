52148664fd18e0b6a21807210c3bfb9eadf3bcf3
Leon <Leon@DESKTOP-Q88U2A7.mshome.net>
2023-06-13 09:36:59 +0200

    latest version

--- NMPC_1Sim.m
+++ NMPC_1Sim.m
@@ -24,27 +24,23 @@ omega0 = 2*pi*420; % [rad/s] equilibrium frequency
 kappa = 16*mu0*Lq*rs^2/(0.82*tau_r*B_pol*pi);
 zeta = m*Cw*tau_A0^2*tau_w*a^3;
 
-Ts = 0.4;
 
-Const = [-4/3*(kappa*Ts*j_BS*w_sat)/(w_sat^2+w_marg^2); Ts*omega0/tau_E0];
-
-%% Non-Linear Discrete Model
-% A = [(1+(4/3)*(kappa*Ts*j_BS*(rho1(x(1),w_marg))))+(C(1,:)/x(1))  0; (-(rho2(x)*Ts)/(zeta))+(C(1,:)/x(1)) (1-Ts/tau_E)];
-% B = [-(kappa*Ts*eta_CD/w_dep)*rho3(x, w_dep); 0];
-
-%% Linearized Discrete Model around w_crit
-N = 20;
+%% Controller and simulation parameters
+N = 20;                % prediction horizon
+Ts = 0.5;              % controller sampling time
 r = [0.06; 5000*2*pi]; % reference state (maybe needs to be different)
-x0 = [0.2;1000*2*pi]; % initial state (low width and high frequency does not need control)
+x0 = [0.2;1000*2*pi];  % initial state (low width and high frequency does not need control)
+Q = [1 0; 0 0];        % no frequency error to reference taken into account!
 
-%%% Check Controlbaility of Linear and Nonlinear Systems %%%
-% If rank(ctrb(A,B)) == nx then the system is controlable
-% rank(ctrb(A,B)); % need to formulate it such that C is part of Ax + Bu
-% rank(ctrb(A0,B0)); % need to formulate it such that C is part of Ax + Bu
+nx = 2; % dimensions of state
+nu = 1; % dimensions of input
 
-%%% Constraints
-min_width = 0.06; % [m] RANDOM VALUE based on 2.4cm deposition width + error in dep. pos.
-max_width = 0.15; % [m]
+k_sim = 1000; % number of simulation time steps
+dt = 1e-2;    % simulation step size
+
+%% Constraints
+min_width = 0.05; % [m] RANDOM VALUE based on 2.4cm deposition width + error in dep. pos.
+max_width = 0.32; % [m]
 min_freq = 100*2*pi;  % [rad/s]
 max_freq = 5000*2*pi; % [rad/s]
 xmin = [min_width;min_freq]; % minimum on state vector
@@ -55,12 +51,9 @@ max_power = 2;    % [MW]
 umin = min_power; % minimum on input vector
 umax = max_power; % maximum on input vector
 
-% Q
-Q = [1 0; 0 0];
 
 %% Nonlinear MPC - YALMIP (solver:fmincon)
-nx = 2;
-nu = 1;
+Const = [-4/3*(kappa*j_BS*w_sat)/(w_sat^2+w_marg^2); omega0/tau_E0];
 x = sdpvar(nx,N+1);
 u = sdpvar(nu,N);
 objective = 0;
@@ -70,38 +63,45 @@ for i = 1:N
     % Use Nonlinear Dynamics
     Amat = A(rho1(x(:,i),w_marg), rho2(x(:,i)), kappa,Ts,j_BS,zeta,tau_E);
     Bmat = B(rho3(x(:,i),w_dep), kappa,Ts,eta_CD,w_dep);
-    constraints = [constraints, x(:,i+1)==Amat*x(:,i)+Bmat*u(:,i)+Const];
+    constraints = [constraints, x(:,i+1)==Amat*x(:,i)+Bmat*u(:,i)+Const*Ts];
     constraints = [constraints, umin<=u(:,i)<=umax]; %xmin<=x(:,i)<=xmax,
 end
 
+% Apply terminal constraints
 objective = objective + (x(:,N+1)-r)'*Q*(x(:,N+1)-r);
 %constraints = [constraints, xmin<=x(:,N+1)<=xmax];
+% Define optimizer
 options = sdpsettings('verbose',1);
 MPC_Nonlinear = optimizer(constraints,objective,options,x(:,1),u);
 % Check the solver of "MPC_Nonlinear": "Solver: FMINCON-STANDARD"
 
-k_sim = 20;
+
+%% Simulation
 xk = [x0 zeros(nx,k_sim)];
 uk = zeros(nu,k_sim);
-tk = zeros(1,k_sim);
 for k = 1:k_sim
-    tic
-    [Uk,~,~,~,~] = MPC_Nonlinear(xk(:,k));
-    tk(:,k) = toc;
-    uk(:,k) = Uk(1:nu);
+    
+    % Every sampling time (starting at 1), adjust controller input
+    if round((k-1)*dt/Ts) == (k-1)*dt/Ts
+        [Uk,~,~,~,~] = MPC_Nonlinear(xk(:,k));
+        uk(:,k) = Uk(1:nu);
+    else
+        uk(:,k) = uk(:,k-1);
+    end
+    
     % Use discrete nonlinear model
-    Amat = A(rho1(xk(:,k),w_marg), rho2(xk(:,k)), kappa,Ts,j_BS,zeta,tau_E);
-    Bmat = B(rho3(xk(:,k),w_dep), kappa,Ts,eta_CD,w_dep);
-    xk(:,k+1) = Amat*xk(:,k)+Bmat*uk(:,k)+Const;
+    xk(:,k+1) = A(rho1(xk(:,k),w_marg), rho2(xk(:,k)), kappa,dt,j_BS,zeta,tau_E)*xk(:,k) ...
+                + B(rho3(xk(:,k),w_dep), kappa,dt,eta_CD,w_dep)*uk(:,k) ...
+                + Const*dt;
 end
 data(1).x = xk;
 data(1).u = uk;
-data(1).t = tk;
 
-%% Plot output and input
+
+%% Plot output and optimal input
 figure('Position', [100 100 1000 300])
 subplot(1,2,1);
-plot(Ts:Ts:k_sim*Ts,uk(:),'color',"#77AC30")
+plot(dt:dt:k_sim*dt,uk(:),'color',"#77AC30")
 xlabel('$t$ [s]','Interpreter','latex')
 ylabel('$P_{ECCD}$ [MW]','Interpreter','latex')
 title("Optimal Input")
@@ -109,11 +109,13 @@ title("Optimal Input")
 subplot(1,2,2);
 hold on
 yyaxis left
-plot(0:Ts:k_sim*Ts,xk(1,:)*100)
+plot(0:dt:k_sim*dt,xk(1,:)*100)
+plot(0:dt:k_sim*dt,r(1)*ones(k_sim+1)*100,'--')
 ylabel('$\mathrm{w}$ [cm]','Interpreter','latex')
 yyaxis right
-plot(0:Ts:k_sim*Ts,xk(2,:)/(2*pi))
+plot(0:dt:k_sim*dt,xk(2,:)/(2*pi))
 ylabel('$\omega$ [Hz]','Interpreter','latex')
 hold off
 xlabel('$t$ [s]','Interpreter','latex')
+legend('Output','Reference')
 title("Output")
