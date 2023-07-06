57790bc6855b47c8a968cfc936b214a8ed700d5d
IsaacSavona <isaacsavona@gmail.com>
2023-07-06 10:52:28 +0200

    reference tracking instead of steady state values

--- NMPC.m
+++ NMPC.m
@@ -31,18 +31,19 @@ Ts = 0.1;             % controller sampling time
 %r = [0.00125; 5000*2*pi]; % reference state (maybe needs to be different)
 %r1 = [0.0835; 5000*2*pi]; % reference state (maybe needs to be different)
 %x0 = [0.06;1000*2*pi]; % initial state (low width and high frequency does not need control)
-x0 = [0.10; 1000*2*pi];
+x0 = [0.04; 1000*2*pi];
 %r1 = [0.04;5000*2*pi];
 %x0 = [0.10; 1000*2*pi];
-r1 = [0.04;5000*2*pi];
-r2 = [0.06;5000*2*pi];
+r1 = [0.086;5000*2*pi];
+r2 = [0.10;5000*2*pi];
 r3 = [0.03;5000*2*pi];
 %x0 = r;
-Q = [200 0; 0 1e-8];        % no frequency error to reference taken into account!
+%Q = [300 0; 0 1e-8];        % no frequency error to reference taken into account!
+Q = [300 0; 0 0];        
 %Q = 100;
 R = 0.0001;
 %R = 0;
-R1 = 0.01;
+R1 = 0.001;
 
 nx = 2; % dimensions of state
 nu = 1; % dimensions of input
@@ -76,6 +77,7 @@ C=[1 0];
 A01 = A(rho1(r1,w_marg), rho2(r1), kappa,Ts,j_BS,zeta,tau_E);
 B01 = B(rho3(r1,w_dep), kappa,Ts,eta_CD,w_dep);
 K1=inv([A01-eye(2) B01; C 0])*[Const; r1(1)];
+%K1(3)=0.516;
 % SS reference 2
 A02 = A(rho1(r2,w_marg), rho2(r2), kappa,Ts,j_BS,zeta,tau_E);
 B02 = B(rho3(r2,w_dep), kappa,Ts,eta_CD,w_dep);
@@ -84,9 +86,12 @@ K2=inv([A02-eye(2) B02; C 0])*[Const; r2(1)];
 A03 = A(rho1(r3,w_marg), rho2(r3), kappa,Ts,j_BS,zeta,tau_E);
 B03 = B(rho3(r3,w_dep), kappa,Ts,eta_CD,w_dep);
 K3=inv([A03-eye(2) B03; C 0])*[Const; r3(1)];
+r = sdpvar(nx,1);
+
 
 for i = 1:N
-    objective = objective + (x(:,i)-K(1:2))'*Q*(x(:,i)-K(1:2)) + (u(:,i)-K(3))'*R*(u(:,i)-K(3)) + (u(:,i)-ustar)'*R1*(u(:,i)-ustar);
+    %objective = objective + (x(:,i)-K(1:2))'*Q*(x(:,i)-K(1:2)) + (u(:,i)-K(3))'*R*(u(:,i)-K(3)) + (u(:,i)-ustar)'*R1*(u(:,i)-ustar);
+    objective = objective + (x(:,i)-r)'*Q*(x(:,i)-r);
     %objective = objective + (C*x(:,i)-K(1))'*Q*(C*x(:,i)-K(1)) + (u(:,i)-K(3))'*R*(u(:,i)-K(3));% + (u(:,i)-u(:,i-1))'*R1*(u(:,i)-u(:,i-1));
     % Use Nonlinear Dynamics
     Amat = A(rho1(x(:,i),w_marg), rho2(x(:,i)), kappa,Ts,j_BS,zeta,tau_E);
@@ -98,12 +103,14 @@ for i = 1:N
 end
 
 % Apply terminal constraints
-objective = objective + (x(:,N+1)-K(1:2))'*Q*(x(:,N+1)-K(1:2));
+%objective = objective + (x(:,N+1)-K(1:2))'*Q*(x(:,N+1)-K(1:2));
+objective = objective + (x(:,N+1)-r)'*Q*(x(:,N+1)-r);
 constraints = [constraints, xmin(2)<=x(2,N+1)<=xmax(2), x(1,N+1)<=xmax(1)];
 % Define optimizer
 options = sdpsettings('usex0',0,'solver','fmincon','verbose',0);
 optimoptions('fmincon','Algorithm','sqp');
-MPC_Nonlinear = optimizer(constraints,objective,options,{x(:,1),ustar,K},u);
+%MPC_Nonlinear = optimizer(constraints,objective,options,{x(:,1),ustar,K},u);
+MPC_Nonlinear = optimizer(constraints,objective,options,{x(:,1),ustar,r},u);
 % Check the solver of "MPC_Nonlinear": "Solver: FMINCON-STANDARD"
 
 
@@ -115,19 +122,26 @@ tk = zeros(1,k_sim);
 ukstar = uk(:,1);
 %U0 = sdpvar(nu,N);
 %assign(u,2);
-Kk = K1;
+%Kk = K1;
+rk = r1;
 for k = 1:k_sim
     k
+    %disp([r1(1),Ref(k)])
     if k == 125
-        Kk = K2;
+        %Kk = K2;
+        rk = r2;
+        %disp([r2(1),Ref(k)])
     end
     if k == 175
-        Kk = K3;
+        %Kk = K3;
+        rk = r3;
+        %disp([r3(1),Ref(k)])
     end
     
     % Every sampling time (starting at 1), adjust controller input
     tic
-    [Uk,~,~,~,~] = MPC_Nonlinear({xk(:,k),ukstar,Kk});
+    %[Uk,~,~,~,~] = MPC_Nonlinear({xk(:,k),ukstar,Kk});
+    [Uk,~,~,~,~] = MPC_Nonlinear({xk(:,k),ukstar,rk});
     tk(k) = toc;
     %assign(u,[Uk(:,2:N),Kk(3)])
     %assign(U0,[Uk(1,2:N);K(3)])
@@ -167,22 +181,22 @@ hold on
 yyaxis left
 plot(0:Ts:k_sim*Ts,xk(1,:)*100)
 %plot(0:k_sim,xk(1,:)*100)
-Ref = [r1(1)*ones(1,124)*100 r2(1)*ones(1,50)*100 r3(1)*ones(1,77)*100];
+Ref = [r1(1)*ones(1,124)*100 r2(1)*ones(1,49)*100 r3(1)*ones(1,78)*100];
 %plot(0:k_sim,Ref,'--')
-%plot(0:Ts:k_sim*Ts,Ref,'--')
-plot(0:Ts:124*Ts,r1(1)*ones(125)*100,'--')
-xline(124*Ts);
-plot(124*Ts:Ts:174*Ts,r2(1)*ones(50+1)*100,'--')
+stairs(0:Ts:k_sim*Ts,Ref,'--')
+% plot(0:Ts:124*Ts,r1(1)*ones(125)*100,'--')
+% xline(124*Ts);
+% plot(124*Ts:Ts:174*Ts,r2(1)*ones(50+1)*100,'--')
 % xline(174*Ts);
 % plot(174*Ts:Ts:k_sim*Ts,r3(1)*ones(75+2)*100,'--')
-% ylabel('$\mathrm{w}$ [cm]','Interpreter','latex')
-% yyaxis right
-% plot(0:Ts:k_sim*Ts,xk(2,:)/(2*pi))
-% ylabel('$\omega$ [Hz]','Interpreter','latex')
-% hold off
-% xlabel('$t$ [s]','Interpreter','latex')
-% legend('Output','Reference')
-% title(sprintf("Output with $\\mathrm{w}_0 =$ %0.3f cm, $\\omega_0 =$ %0.0f Hz",x0(1)*100,x0(2)/2/pi),'Interpreter','latex')
+ylabel('$\mathrm{w}$ [cm]','Interpreter','latex')
+yyaxis right
+plot(0:Ts:k_sim*Ts,xk(2,:)/(2*pi))
+ylabel('$\omega$ [Hz]','Interpreter','latex')
+hold off
+xlabel('$t$ [s]','Interpreter','latex')
+legend('Output','Reference')
+title(sprintf("Output with $\\mathrm{w}_0 =$ %0.3f cm, $\\omega_0 =$ %0.0f Hz",x0(1)*100,x0(2)/2/pi),'Interpreter','latex')
 
 subplot(2,2,[2,4]);
 plot(1:k_sim,data(1).t,'color',"#7E2F8E")
